432f5f832e1f2abd43f821a16ed6ebd3170d36b9
Nawale Gaurav Raju <nawalegaurav26@gmail.com>
2026-04-21 00:34:33 +0530

    chore: remove unused React import in BackgroundAnimation

--- src/components/BackgroundAnimation.tsx
+++ src/components/BackgroundAnimation.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import { useEffect, useRef } from 'react';
 
 const BackgroundAnimation = () => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
